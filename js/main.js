f1e80443a205021437edf92229ecefcba58c6048
ErrSys <teamgamingv2@gmail.com>
2023-09-09 20:30:24 +0100

    Finished HTML and scroll scripts!

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let scrollIndex = 0;
+window.addEventListener('wheel', event => scrollDetector(event));
+
+const scrollDetector = event => {
+    if (event.deltaY == 100) {
+        scrollDown();
+    } else {
+        scrollUp();
+    }
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+}
+
+const scrollUp = () => {
+    const sliderContener = document.querySelector('.slider');
+    if (scrollIndex != 0) {
+        const newSlide = sliderContener.children[scrollIndex-1];
+        const currentSlide = sliderContener.children[scrollIndex];
+        scrollIndex--;
+        sliderContener.style.animation = 'fadeOut 0.5s';
+        setTimeout(() => {
+            newSlide.className = '';
+            currentSlide.className = 'hidden';
+            sliderContener.style.opacity = 0; 
+            sliderContener.style.animation = 'fadeIn 0.5s';
+            setTimeout(() => {
+                sliderContener.style.opacity = 1;
+            }, 500);
+        }, 250);
+    }
+}
+
+const scrollDown = () => {
+    const sliderContener = document.querySelector('.slider');
+    if (scrollIndex < sliderContener.children.length -2) {
+        const newSlide = sliderContener.children[scrollIndex+1];
+        const currentSlide = sliderContener.children[scrollIndex];
+        scrollIndex++;
+        sliderContener.style.animation = 'fadeOut 0.5s';
+        setTimeout(() => {
+            newSlide.className = '';
+            currentSlide.className = 'hidden';
+            sliderContener.style.opacity = 0; 
+            sliderContener.style.animation = 'fadeIn 0.5s';
+            setTimeout(() => {
+                sliderContener.style.opacity = 1;
+            }, 500);
+        }, 250);
+    }
+}
